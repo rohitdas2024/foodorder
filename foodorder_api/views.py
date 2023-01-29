b3596aa06f24ebed3094ea9ced30d9570672573b
Rohit Das <rohitdas2024@gmail.com>
2023-01-29 12:34:46 +0530

    modified:   foodorder_api/views.py

--- foodorder_api/views.py
+++ foodorder_api/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Outlet,Item
 from .serializers import OutletSerializer,ItemSerializer
+from rest_framework import permissions
 
 class ListOutlet(generics.ListCreateAPIView):
+  permission_classes=(permissions.IsAuthenticated)
   queryset=Outlet.objects.all()
   serializer_class=OutletSerializer
 
 class DetailOutlet(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes=(permissions.IsAuthenticated)
   queryset=Outlet.objects.all()
   serializer_class=OutletSerializer
 
 class ListItem(generics.ListCreateAPIView):
+  permission_classes=(permissions.IsAuthenticated)
   queryset=Item.objects.all()
   serializer_class=ItemSerializer
 
 class DetailItem(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes=(permissions.IsAuthenticated)
   queryset=Item.objects.all()
   serializer_class=ItemSerializer
